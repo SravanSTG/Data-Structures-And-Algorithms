cb17dd562f23dfe589e380dca1f5ffc5ebc20d59
Sravan <53626426+SravanSTG@users.noreply.github.com>
2021-05-23 15:31:49 +0530

    Add files via upload

---
+++ DataStructures/Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ReverseAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hi, my name is Sravan";
+            string revStr = "";
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                revStr += str[i];
+            }
+
+            // Another way
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+
+            Console.WriteLine(str);
+            Console.WriteLine(revStr);
+            Console.WriteLine(charArray);
+
+            Console.Read();
+        }
+    }
+}
