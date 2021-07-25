d63468b426dc3c9cf58f2a54c37d12965a3f1edc
Sravan <53626426+SravanSTG@users.noreply.github.com>
2021-07-25 11:52:27 +0530

    Add files via upload

---
+++ Algorithms/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static long FactorialLoop(int n)
+        {
+            long fact = 1;
+            while (n > 0)
+            {
+                fact *= n;
+                n--;
+            }
+
+            return fact;
+        }
+
+        static long FactorialRecursive(int n)
+        {
+            if (n == 1)
+                return n;
+            else
+                return n * FactorialRecursive(n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            long res, rec;
+            res = FactorialLoop(5);
+            rec = FactorialRecursive(6);
+            Console.WriteLine(res);
+            Console.WriteLine(rec);
+
+            Console.Read();
+        }
+    }
+}
