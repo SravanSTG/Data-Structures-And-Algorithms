874ef9e04834c1a4b41ad34b285a4e3a038b8298
Sravan <53626426+SravanSTG@users.noreply.github.com>
2021-07-29 19:55:49 +0530

    Add files via upload

---
+++ Algorithms/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0 };
+            int[] resArr = BubbleSort(arr);
+            foreach (int item in resArr)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.Read();
+        }
+
+        static int[] BubbleSort(int[] arr)
+        {
+            int temp;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+}
